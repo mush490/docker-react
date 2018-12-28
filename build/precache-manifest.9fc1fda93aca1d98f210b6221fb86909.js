4010ea369ddcef41fe70ad4b9527c4496fab6e93
Michael Michaud <michael.michaud@espn.com>
2018-12-28 07:04:53 -0500

    initial commit

---
+++ build/precache-manifest.9fc1fda93aca1d98f210b6221fb86909.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8c55c5222c50dcf77a6",
+    "url": "/static/js/main.f8c55c52.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f8c55c5222c50dcf77a6",
+    "url": "/static/css/main.5b42c735.chunk.css"
+  },
+  {
+    "revision": "27fe8a1bd4bf4da5398a691f9de31bdd",
+    "url": "/index.html"
+  }
+];
